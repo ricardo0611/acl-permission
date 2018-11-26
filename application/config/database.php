430c9ac1a47fab23a5728c6d19c7daf692dbcac4
Deividdy Nogueira <deividdyn@gmail.com>
2018-11-26 17:21:19 -0200

    set db conf

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.120.250',
+	'username' => 'root',
+	'password' => 'ibulla',
+	'database' => 'my_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
